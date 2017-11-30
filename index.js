ea516474c10c06c4937a698522a0becfcc302268
Eduard <esimonyan2014@gmail.com>
2017-11-30 19:16:22 +0400

    enabled cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const winston = require('winston');
 const logger = require('morgan');
+const cors = require('cors');
 const config = require('./config');
 const auth = require('./middlewares/auth').isAuth;
 process.env.NODE_ENV = config.mode;
@@ -29,7 +30,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.static(`${__dirname}/build`));
-
+app.use(cors());
 app.use((req, res, next) => {
     if ("OPTIONS" === req.method) {
         if (req.headers["access-control-request-headers"]) {
